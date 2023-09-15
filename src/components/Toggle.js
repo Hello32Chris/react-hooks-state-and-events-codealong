1b0b46deb6cd2e55938204262039758c6c8d7c2f
Christopher Orth <christopherorth430@yahoo.com>
2023-09-15 11:27:03 -0400

    fun lab with state

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOn, setIson] = useState(false)
+
+  function handleClick() {
+    setIson((isOn) => !isOn)
+  }
+
+  const color = isOn ? "red" : "white"
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "OFF"  : "ON"}
+    </button>
+  )
 }
 
 export default Toggle;
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------THIS IS MY CODE-ALONG ABOE IS MYSELF PRACTICING---------
+
+// import React, { useState } from "react";
+
+// function Toggle() {
+
+//   const [isOn, setIson] = useState(false);
+
+//   function handleClick(){
+//     setIson((ison) => !isOn)
+//   };
+
+//   return <button onClick={handleClick} >{isOn ? "ON" : "OFF"}</button>;
+// }
+
+// export default Toggle;
